86b0ac94c6e7a778e0d8f1ed6cb4b643cca05a2c
Matthew Crossley <matthew.crossley@thomsonreuters.com>
2026-06-10 17:03:41 +0100

    More deployment tweaks

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,2 @@
-export const ENV = process.env.ENV ?? 'dev'
-export const BACKEND = process.env.BACKEND_URL ?? 'http://localhost:8000'
+export const ENV = process.env.NODE_ENV ?? 'development'
+export const BACKEND = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8000'
