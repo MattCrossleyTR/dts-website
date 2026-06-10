c57ca0130f7d598190e3336d0e799108e3992ba6
Matthew Crossley <matthew.crossley@thomsonreuters.com>
2026-06-10 13:28:05 +0100

    Add some filtering to homepage

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
-import { getAuthToken } from "../utils/auth";
+import { getAuthPayload, getAuthToken } from "../utils/auth";
 import Task from "../components/task";
 import "../styles/task.css";
 
 export default function HomePage() {
   const [tasks, setTasks] = useState([]);
+  const [filteredTasks, setFilteredTasks] = useState([]);
+
+  const currentUserId = getAuthPayload().sub
+  const [filterMode, setFilterMode] = useState("all")
 
   useEffect(() => {
     fetch("http://localhost:8000/tasks/", {
@@ -17,11 +21,34 @@ export default function HomePage() {
       .catch((error) => console.error("Error fetching tasks:", error));
   }, []);
 
+  useEffect(() => {
+    const filteredTasks = tasks.filter(t => {
+      switch (filterMode) {
+        case "createdByMe":
+          return t.created_by === currentUserId
+        case "assignedToMe":
+          return t.assigned_to === currentUserId
+        default:
+          return true
+      }
+    })
+    setFilteredTasks(filteredTasks)
+  }, [tasks, filterMode])
+
   return (
     <div className="task-container">
       <h1>Your Tasks</h1>
+      <div>
+        <span>Filter by:</span>
+        <input type="radio" name="filter" id="all" onClick={() => setFilterMode("all")} checked={filterMode === "all"} />
+        <label for="all">All tasks</label>
+        <input type="radio" name="filter" id="createdByMe" onClick={() => setFilterMode("createdByMe")} checked={filterMode === "createdByMe"} />
+        <label for="createdByMe">Created by me</label>
+        <input type="radio" name="filter" id="assignedToMe" onClick={() => setFilterMode("assignedToMe")} checked={filterMode === "assignedToMe"} />
+        <label for="assignedToMe">Assigned to me</label>
+      </div>
       <div className="task-list">
-        {Array.isArray(tasks) && tasks.map((task) => <Task task={task} />)}
+        {Array.isArray(filteredTasks) && filteredTasks.map((task) => <Task task={task} />)}
       </div>
     </div>
   );
