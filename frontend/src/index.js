736f3b7492851ff4e6df169662b3941703a68d60
Matthew Crossley <matthew.crossley@thomsonreuters.com>
2026-06-11 14:43:31 +0100

    Use hash router for better navigation on github pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
-import { BrowserRouter } from 'react-router-dom';
-import { BASE_URL } from './constants';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log("BAEURL", BASE_URL)
 root.render(
   <React.StrictMode>
     {/* wrap whole app in router so we have access to useLocation throughout the app */}
-    <BrowserRouter basename={BASE_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
