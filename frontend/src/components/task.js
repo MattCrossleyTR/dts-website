be4c056abdbd717168cfa3b1007f31ac628b6011
Matthew Crossley <matthew.crossley@thomsonreuters.com>
2026-06-11 14:20:53 +0100

    Ensure base url respected

--- frontend/src/components/task.js
+++ frontend/src/components/task.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Task({ task }) {
   return (
-    <a href={`/task/${task.id}`}>
+    <Link to={`/task/${task.id}`}>
       <div class="task">
         <h3>[{task.completed ? "✓" : " "}] {task.title}</h3>
         <p>{task.description}</p>
         <p>Due: {task.due ? new Date(task.due).toLocaleString() : "No due date"}</p>
       </div>
-    </a>
+    </Link>
   );
 }
