fc8a28bc3984aa2f725ffe00333d29395f4dc954
Matthew Crossley <matthew.crossley@thomsonreuters.com>
2026-06-11 09:38:43 +0100

    Chaneg base url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,3 @@
 export const ENV = process.env.NODE_ENV ?? 'development'
 export const BACKEND = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8000'
+export const BASE_URL = process.env.REACT_APP_BASE_URL ?? '/'
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import { BrowserRouter } from 'react-router-dom';
+import { BASE_URL } from './constants';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log("BAEURL", BASE_URL)
 root.render(
   <React.StrictMode>
     {/* wrap whole app in router so we have access to useLocation throughout the app */}
-    <BrowserRouter>
+    <BrowserRouter basename={BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
